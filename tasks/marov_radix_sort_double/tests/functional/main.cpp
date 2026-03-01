3ea86f241b4ed0f082142e1d3e5fe4c57463e912
marofal <marov2021@vk.com>
2026-03-02 00:46:18 +0300

    marov: task 3 - radix sort double (MPI + SEQ)

--- tasks/marov_radix_sort_double/tests/functional/main.cpp
+++ tasks/marov_radix_sort_double/tests/functional/main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
-
-#include <algorithm>
+#include <array>
+#include <string>
 #include <tuple>
 #include <vector>
 
@@ -8,13 +8,14 @@
 #include "marov_radix_sort_double/mpi/include/ops_mpi.hpp"
 #include "marov_radix_sort_double/seq/include/ops_seq.hpp"
 #include "util/include/func_test_util.hpp"
-#include "util/include/util.hpp"
 
 namespace marov_radix_sort_double {
 
-class MarovRadixSortDoubleFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class MarovRadixSortDoubleFuncTests
+    : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
-  static std::string PrintTestParam(const testing::TestParamInfo<ParamType>& info) {
+  static std::string PrintTestParam(
+      const testing::TestParamInfo<ParamType>& info) {
     return "test_" + std::to_string(info.index);
   }
 
@@ -45,7 +46,8 @@ TEST_P(MarovRadixSortDoubleFuncTests, RadixSortDoubleSimpleMerge) {
 }
 
 const std::array kTestCases = {
-    std::make_tuple(std::vector<double>{5.0, 2.0, 8.0, 1.0, 9.0}, std::vector<double>{1.0, 2.0, 5.0, 8.0, 9.0}),
+    std::make_tuple(std::vector<double>{5.0, 2.0, 8.0, 1.0, 9.0},
+                    std::vector<double>{1.0, 2.0, 5.0, 8.0, 9.0}),
     std::make_tuple(std::vector<double>{}, std::vector<double>{}),
     std::make_tuple(std::vector<double>{1.0}, std::vector<double>{1.0}),
     std::make_tuple(std::vector<double>{3.0, 1.0, 4.0, 1.0, 5.0, 9.0, 2.0, 6.0},
@@ -56,13 +58,16 @@ const std::array kTestCases = {
                     std::vector<double>{0.5, 1.5, 2.1, 3.7, 4.9}),
 };
 
-const auto kAllTestTasks = std::tuple_cat(
-    ppc::util::AddFuncTask<MarovRadixSortDoubleMPI, InType>(kTestCases, PPC_SETTINGS_marov_radix_sort_double),
-    ppc::util::AddFuncTask<MarovRadixSortDoubleSEQ, InType>(kTestCases, PPC_SETTINGS_marov_radix_sort_double));
+const auto kAllTestTasks =
+    std::tuple_cat(ppc::util::AddFuncTask<MarovRadixSortDoubleMpi, InType>(
+                       kTestCases, PPC_SETTINGS_marov_radix_sort_double),
+                   ppc::util::AddFuncTask<MarovRadixSortDoubleSeq, InType>(
+                       kTestCases, PPC_SETTINGS_marov_radix_sort_double));
 
-INSTANTIATE_TEST_SUITE_P(RadixSortDoubleFuncTests, MarovRadixSortDoubleFuncTests,
-                         ppc::util::ExpandToValues(kAllTestTasks),
-                         MarovRadixSortDoubleFuncTests::PrintTestParam);
+INSTANTIATE_TEST_SUITE_P(
+    RadixSortDoubleFuncTests, MarovRadixSortDoubleFuncTests,
+    ppc::util::ExpandToValues(kAllTestTasks),
+    MarovRadixSortDoubleFuncTests::PrintTestParam);
 
 }  // namespace
 }  // namespace marov_radix_sort_double
